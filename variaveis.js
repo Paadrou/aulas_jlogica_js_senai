ef3531680f9dabf76ca2fbe1b43201d93ae588f9
IGOR SILVA SCHIMITZ <igor_s_schimitz@estudante.sesisenai.org.br>
2025-04-16 22:18:26 -0300

    feat:console.js e variaveis.js

---
+++ variaveis.js
@@ -0,0 +1,19 @@
+/*var nome = "Igor" 
+
+var nome = "BOSTAAAAAA"*/
+
+    //var
+    
+/* let nome = "igor"
+
+nome = "bosta" */
+
+    //let
+
+const nome = "igor"
+
+nome = "fwafaWF"
+
+// const não pode mudar de valor 
+
+console.log(nome)
